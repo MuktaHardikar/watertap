7c46e88db4eb886cf1957637190d74e1060cf79d
MuktaHardikar <123592712+MuktaHardikar@users.noreply.github.com>
2025-09-30 11:22:59 -0600

    updates to component files

--- watertap/flowsheets/flex_desal/wrd/components/chemical_addition.py
+++ watertap/flowsheets/flex_desal/wrd/components/chemical_addition.py
@@ -34,51 +34,36 @@ from watertap.core.solvers import get_solver
 from watertap.core import Database
 from watertap.unit_models.zero_order import ChemicalAdditionZO
 from watertap.core.wt_database import Database
-from watertap.core.zero_order_properties import WaterParameterBlock as ZO
+from watertap.core.zero_order_properties import WaterParameterBlock
 from watertap.core.util.model_diagnostics.infeasible import *
-from watertap.property_models.multicomp_aq_sol_prop_pack import MCASParameterBlock
+from watertap.costing.zero_order_costing import ZeroOrderCosting
 from watertap.core.util.initialization import *
-
-
-import yaml
-import os
-
-
-rho = 1000 * pyunits.kg / pyunits.m**3
 
 
 def build_system():
     
     m = ConcreteModel()
-    m.db = Database(dbpath='watertap/flowsheets/3.25/WRD/meta_data')
     m.fs = FlowsheetBlock(dynamic=False)
-    m.fs.properties = ZO(solute_list=["tds"])
-    m.fs.feed = Feed(property_package=m.fs.properties)
-    m.fs.product = Product(property_package=m.fs.properties)
+
+    m.db = Database(dbpath='watertap/flowsheets/flex_desal/wrd/meta_data')
+    m.fs.properties = WaterParameterBlock(
+            solute_list=["tds","tss"]
+        )
 
     m.fs.chem_addition = FlowsheetBlock(dynamic=False)
 
-    build_chem_addition(m, m.fs.chem_addition, "ammonia", m.fs.properties)
-
-    m.fs.feed_to_chem = Arc(
-        source=m.fs.feed.outlet,
-        destination=m.fs.chem_addition.feed.inlet,
-    )
-
-    m.fs.chem_to_product = Arc(
-        source=m.fs.chem_addition.product.outlet,
-        destination=m.fs.product.inlet,
-    )
+    build_chem_addition(m.fs.chem_addition, "ammonia", m.fs.properties)
 
     TransformationFactory("network.expand_arcs").apply_to(m)
 
     return m
 
 
-def build_chem_addition(m, blk, chemical_name, prop_package=None) -> None:
+def build_chem_addition(blk, chemical_name, prop_package=None) -> None:
 
     print(f'\n{"=======> BUILDING CHEMICAL ADDITION SYSTEM <=======":^60}\n')
 
+    m = blk.model()
     if prop_package is None:
         prop_package = m.fs.properties
 
@@ -100,25 +85,14 @@ def build_chem_addition(m, blk, chemical_name, prop_package=None) -> None:
         destination=blk.product.inlet,
     )
 
-    TransformationFactory("network.expand_arcs").apply_to(m)
+
+def set_system_conditions(blk):
+    blk.feed.properties[0.0].flow_mass_comp["H2O"].fix(171.37)
+    blk.feed.properties[0.0].flow_mass_comp["tds"].fix(600)
+    blk.feed.properties[0.0].flow_mass_comp["tss"].fix(5.22e-6)
 
 
-def set_system_operating_conditions(m, Qin=5, tds=130):
-    print(
-        "\n\n-------------------- SETTING SYSTEM OPERATING CONDITIONS --------------------\n\n"
-    )
-    Qin = Qin * pyunits.Mgal / pyunits.day
-    tds = tds * pyunits.kg / pyunits.m**3
-
-    flow_mass_water = pyunits.convert(Qin * rho, to_units=pyunits.kg / pyunits.s)
-    flow_mass_tds = pyunits.convert(Qin * tds, to_units=pyunits.kg / pyunits.s)
-
-    m.fs.feed.properties[0].flow_mass_comp["H2O"].fix(flow_mass_water)
-    m.fs.feed.properties[0].flow_mass_comp["tds"].fix(flow_mass_tds)
-    m.fs.chem_addition.unit.properties[0].flow_mass_comp["tds"].set_value(flow_mass_tds)
-
-
-def set_chem_addition_scaling(m, blk, calc_blk_scaling_factors=False):
+def set_chem_addition_scaling(blk, calc_blk_scaling_factors=False):
 
     set_scaling_factor(blk.unit.chemical_dosage, 0.1)
     set_scaling_factor(blk.unit.solution_density, 1e-3)
@@ -130,68 +104,40 @@ def set_chem_addition_scaling(m, blk, calc_blk_scaling_factors=False):
     if calc_blk_scaling_factors:
         calculate_scaling_factors(blk)
 
-    # otherwise calculate all scaling factors
-    else:
-        calculate_scaling_factors(m)
 
-
-def set_chem_addition_op_conditions(m, blk, **kwargs):
-
+def set_chem_addition_op_conditions(blk, **kwargs):
+    m = blk.model()
+    m.db.get_unit_operation_parameters("chemical_addition")
     blk.unit.load_parameters_from_database()
-    for k, v in kwargs.items():
-        print(blk.name, k)
-        try:
-            vv = getattr(blk.unit, k)
-        except:
-            continue
-        print(f"{blk.name} {vv.name} {v}")
-        if isinstance(vv, Var):
-            vv.fix(v)
-        if isinstance(vv, Param):
-            vv.set_value(v)
 
-    print(f"Chem Addition")
-    print(f"\tblock DOF = {degrees_of_freedom(blk)}\n")
-    print(f"\tunit DOF = {degrees_of_freedom(blk.unit)}\n")
+def add_costing(m):
+    m.fs.costing = ZeroOrderCosting(case_study_definition = pathlib.Path('watertap/flowsheets/flex_desal/wrd/meta_data/wrd_case_study.yaml'))
 
 
 def add_chem_addition_costing(m, blk, flowsheet_costing_block=None):
     if flowsheet_costing_block is None:
         flowsheet_costing_block = m.fs.costing
+
     blk.unit.costing = UnitModelCostingBlock(
         flowsheet_costing_block=flowsheet_costing_block
     )
 
 
-def init_system(blk, solver=None, flow_out=None):
+def init_chem_addition(blk, solver=None):
     if solver is None:
         solver = get_solver()
-    if flow_out is None:
-        flow_out = blk.fs.feed.properties[0].flow_vol
 
-    print("\n\n-------------------- INITIALIZING SYSTEM --------------------\n\n")
-    print(f"System Degrees of Freedom: {degrees_of_freedom(m)}")
-    print(f"Chem Addition Degrees of Freedom: {degrees_of_freedom(m.fs.chem_addition)}")
+    # print("\n\n-------------------- INITIALIZING SYSTEM --------------------\n\n")
+    # print(f"System Degrees of Freedom: {degrees_of_freedom(m)}")
+    # print(f"Chem Addition Degrees of Freedom: {degrees_of_freedom(m.fs.chem_addition)}")
 
-    m.fs.feed.initialize()
-    propagate_state(m.fs.feed_to_chem)
-    init_chem_addition(m, m.fs.chem_addition)
-    propagate_state(m.fs.chem_to_product)
-    m.fs.product.initialize()
-
-    # m.fs.costing.cost_process()
-    # m.fs.costing.initialize()
-    # m.fs.costing.add_LCOW(flow_out)
-
-
-def init_chem_addition(m, blk):
     blk.feed.initialize()
     propagate_state(blk.feed_to_unit)
+
     blk.unit.initialize()
+
     propagate_state(blk.unit_to_product)
-    print(f"Chem Addition")
-    print(f"\tblock DOF after init = {degrees_of_freedom(blk)}\n")
-    print(f"\tunit DOF after init = {degrees_of_freedom(blk.unit)}\n")
+    blk.product.initialize()
 
 
 def print_chem_addition_costing_breakdown(blk):
@@ -226,12 +172,18 @@ def solve(m, solver=None, tee=True, raise_on_failure=True):
 
 if __name__ == "__main__":
     m = build_system()
-    set_system_operating_conditions(m)
-    set_chem_addition_op_conditions(m, m.fs.chem_addition)
-    # add_chem_addition_costing(m, m.fs.chem_addition)
-    set_chem_addition_scaling(m, m.fs.chem_addition, calc_blk_scaling_factors=True)
-    init_system(m)
+    set_system_conditions(m.fs.chem_addition)
+    set_chem_addition_op_conditions(m.fs.chem_addition)
+    set_chem_addition_scaling(m.fs.chem_addition, calc_blk_scaling_factors=True)
+    init_chem_addition(m.fs.chem_addition)
     solve(m)
-    # print_chem_addition_costing_breakdown(m.fs.chem_addition)
-    # print(f"LCOW = {m.fs.costing.LCOW()}")
-    print("Ammonia added:", m.fs.chem_addition.unit.chemical_flow_vol[0]())
+    
+    add_costing(m)
+    add_chem_addition_costing(m, m.fs.chem_addition)
+
+    m.fs.costing.cost_process()
+    m.fs.costing.initialize()
+
+    solve(m)
+
+    m.fs.costing.display()
---
+++ watertap/flowsheets/flex_desal/wrd/wrd_treatment_train.py
@@ -0,0 +1,174 @@
+from pyomo.environ import (
+    ConcreteModel,
+    Param,
+    check_optimal_termination,
+    value,
+    assert_optimal_termination,
+    units as pyunits,
+    value,
+    TransformationFactory,
+)
+
+import pyomo.contrib.parmest.parmest as parmest
+from pyomo.contrib.parmest.experiment import Experiment
+
+from idaes.core.util.initialization import propagate_state
+from idaes.core.util.model_statistics import degrees_of_freedom
+from idaes.core import FlowsheetBlock
+from idaes.models.unit_models import MixingType, MomentumMixingType, Mixer, Separator, Product, Feed
+
+from watertap.property_models.NaCl_T_dep_prop_pack import NaClParameterBlock
+from watertap.core.zero_order_properties import WaterParameterBlock
+
+from watertap.flowsheets.flex_desal.wrd.components.UF import *
+from watertap.flowsheets.flex_desal.wrd.components.chemical_addition import *
+from watertap.flowsheets.flex_desal.wrd.components.ro_system import *
+from watertap.flowsheets.flex_desal.wrd.components.translator_ZO_to_NaCl import TranslatorZOtoNaCl
+
+# TODO:
+# Update RO inlet conditions based on translator output
+# Add UF sludge?
+# RO is not optimal solution
+
+def build_wrd_system():
+    m = ConcreteModel()
+    m.fs = FlowsheetBlock(dynamic=False)
+
+    m.db = Database(dbpath='watertap/flowsheets/flex_desal/wrd/meta_data')
+
+    m.fs.properties = WaterParameterBlock(
+            solute_list=["tds","tss"]
+        )
+    
+    # RO properties
+    m.fs.ro_properties = NaClParameterBlock()
+
+    # Add units
+    m.fs.feed = Feed(property_package=m.fs.properties)
+    m.fs.product= Product(property_package=m.fs.properties)
+
+    # Chemical addition units
+    m.fs.ammonia_addition = FlowsheetBlock(dynamic=False)
+    build_chem_addition(m.fs.ammonia_addition, "ammonia", m.fs.properties)
+
+    m.fs.hypochlorite_addition = FlowsheetBlock(dynamic=False)
+    build_chem_addition(m.fs.hypochlorite_addition, "sodium_hypochlorite", m.fs.properties)
+
+    # UF unit
+    m.fs.UF = FlowsheetBlock(dynamic=False)
+    build_UF(m.fs.UF, m.fs.properties)
+
+    # Translator block between ZO to RO property packages
+    m.fs.translator_ZO_to_RO = TranslatorZOtoNaCl(
+        inlet_property_package=m.fs.properties,
+        outlet_property_package=m.fs.ro_properties,
+        has_phase_equilibrium=False,
+        outlet_state_defined=True,
+    )
+
+    # RO unit
+    m.fs.ro_train = FlowsheetBlock(dynamic=False)
+    build_wrd_ro_system(m.fs.ro_train, prop_package=m.fs.ro_properties)
+
+    # Translator block between RO to ZO property packages
+
+    return m
+
+def add_connections(m):
+    # Connect feed to ammonia addition
+    m.fs.s01 = Arc(source=m.fs.feed.outlet, destination=m.fs.ammonia_addition.feed.inlet)
+    # Connect ammonia addition to hypochlorite addition
+    m.fs.s02 = Arc(source=m.fs.ammonia_addition.product.outlet, destination=m.fs.hypochlorite_addition.feed.inlet)
+    # Connect hypochlorite addition to UF
+    m.fs.s03 = Arc(source=m.fs.hypochlorite_addition.product.outlet, destination=m.fs.UF.feed.inlet)
+    # Connect UF to RO translator
+    m.fs.s04 = Arc(source=m.fs.UF.product.outlet, destination=m.fs.translator_ZO_to_RO.inlet)
+    # Connect RO translator to RO
+    m.fs.s05 = Arc(source=m.fs.translator_ZO_to_RO.outlet, destination=m.fs.ro_train.total_ro_feed.inlet)
+
+    TransformationFactory("network.expand_arcs").apply_to(m)
+
+
+def set_wrd_inlet_conditions(m):
+    # Inlet conditions
+    # m.fs.feed.properties[0].pressure.fix(101325)  # Fix feed pressure to 1 atm
+    # m.fs.feed.properties[0].temperature.fix(298.15)  # Fix feed temperature to 25 C
+    m.fs.feed.properties[0].flow_mass_comp["H2O"].fix(174)  # Fix feed water flow rate
+    m.fs.feed.properties[0].flow_mass_comp["tds"].fix(0.05)  # Fix feed salt flow rate
+    m.fs.feed.properties[0].flow_mass_comp["tss"].fix(0.1)  # Fix feed salt flow rate
+
+
+def set_wrd_operating_conditions(m):
+    # Operating conditions
+    set_chem_addition_op_conditions(blk=m.fs.ammonia_addition)
+    set_chem_addition_op_conditions(blk=m.fs.hypochlorite_addition)
+    set_UF_op_conditions(m.fs.UF)
+    set_ro_operation_conditions(m.fs.ro_train)
+
+
+def initialize_wrd_system(m):
+
+    m.fs.feed.initialize()
+    propagate_state(m.fs.s01)
+    init_chem_addition(m.fs.ammonia_addition)
+    propagate_state(m.fs.s02)
+    init_chem_addition(m.fs.hypochlorite_addition)
+    propagate_state(m.fs.s03)
+    init_UF(m.fs.UF)
+    propagate_state(m.fs.s04)
+    m.fs.translator_ZO_to_RO.initialize()
+    propagate_state(m.fs.s05)
+    m.fs.ro_train.total_ro_feed.initialize()
+    build_ro_inlet_stream(m.fs.ro_train, test=False)
+    initialize_ro_units(m.fs.ro_train)
+
+def set_wrd_system_scaling(m):
+
+    set_chem_addition_scaling(blk=m.fs.ammonia_addition )
+    set_chem_addition_scaling(blk=m.fs.hypochlorite_addition)
+    add_UF_scaling(m.fs.UF)
+    add_ro_scaling(m.fs.ro_train)
+
+
+def solve(model, solver=None, tee=True, raise_on_failure=True):
+    # ---solving---
+    if solver is None:
+        solver = get_solver()
+
+    print("\n--------- SOLVING ---------\n")
+
+    results = solver.solve(model, tee=tee)
+
+    if check_optimal_termination(results):
+        print("\n--------- OPTIMAL SOLVE!!! ---------\n")
+        return results
+    msg = (
+        "The current configuration is infeasible. Please adjust the decision variables."
+    )
+    if raise_on_failure:
+        raise RuntimeError(msg)
+    else:
+        return results
+
+
+if __name__ == "__main__":
+
+    m = build_wrd_system()
+    add_connections(m)
+
+    set_wrd_inlet_conditions(m)
+    set_wrd_operating_conditions(m)
+
+    set_wrd_system_scaling(m)
+    initialize_wrd_system(m)
+
+    m.fs.ro_train.total_ro_feed.display()
+    m.fs.ro_train.feed.display()  
+
+    # assert False  
+
+    try:
+        results = solve(m)
+        assert_optimal_termination(results)
+    except:
+        print_infeasible_constraints(m)
